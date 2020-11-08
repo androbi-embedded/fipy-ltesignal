4f68b0a75675569938eaa9ba6a9ee1bafdfa5e32
Claus Denk <denk@us.es>
2020-11-08 17:52:09 +0100

    add RSRP

--- main.py
+++ main.py
@@ -5,19 +5,41 @@ socket.dnsserver(0,"172.31.16.100")
 socket.dnsserver(1,"172.31.32.100")
 import time
 import re
+import sys
 import network
 import pycom
 import math
 
-def signal_quality(show_status=False, show_at=False, sensitivity=False):
+def signal_quality(rsrp_center=-110, rsrp_hw=15, rsrq_center=-15, rsrq_hw=7.5,
+    show_status=False, show_at=False, sensitivity=False):
     """ attach to LTE and then query signal quality """
-    pycom.heartbeat(True)
-    lte = network.LTE()
-    lte.reset()
-    lte.init()
-    lte.attach(band=20, apn="pycom.io") 
+    # init mean values
+    rsrp_mean=0.0
+    rsrq_mean=0.0
+    valid_data=0
+    no_cell=0
+    # calculate intervals and optionally display
+    rsrp_min=rsrp_center - rsrp_hw
+    rsrp_max=rsrp_center + rsrp_hw
+    rsrq_min=rsrq_center - rsrq_hw
+    rsrq_max=rsrq_center + rsrq_hw
     if show_status:
-        print("attaching..",end='')
+        print("rsrp [",rsrp_min,",",rsrp_max,"] center:",rsrp_center)
+        print("rsrq [",rsrq_min,",",rsrq_max,"] center:",rsrq_center)
+    # init and attach
+    pycom.heartbeat(True)
+    if show_status:
+        print("init lte")
+    try:
+        lte = network.LTE()
+        lte.reset()
+    except OSError:
+        print('cannot init/reset lte. switch your device off and on again.')
+        sys.exit()
+    lte.init()
+    if show_status:
+        print("attaching",end='')
+    lte.attach(band=20, apn="pycom.io") 
     while not lte.isattached():
         time.sleep(0.25)
         if show_status:
@@ -26,16 +48,48 @@ def signal_quality(show_status=False, show_at=False, sensitivity=False):
             print(lte.send_at_cmd('AT!="fsm"'))         # get the System FSM
     if show_status:
         print("attached!")
-    
     pycom.heartbeat(False)
+    for _ in range(3):
+        pycom.rgbled(0x00ff00)
+        time.sleep(0.15)
+        pycom.rgbled(0)
+        time.sleep(0.05)
+    time.sleep(2)
     while True:
         res = lte.send_at_cmd('AT!="showphy"')
-        match = re.search(r'RSRQ\s\s\(dB\)\s*:\s*(-?[0-9]*\.[0-9]*)', res) 
-        rsrq = 0.0
-        if match:
-            rsrq = float(match.group(1))
-        color_strength(rsrq, sensitivity)
-        time.sleep(1.0)
+        #print(res)
+        if res.find('CELL_ACQUIRED') != -1:
+            match = re.search(r'RSRP\s\(dBm\)\s*:\s*(-?[0-9]*\.[0-9]*)\s*RSRQ\s\s\(dB\)\s*:\s*(-?[0-9]*\.[0-9]*)', res) 
+            rsrp = 0.0
+            rsrq = 0.0
+            if match:
+                no_cell = 0
+                rsrp = float(match.group(1))
+                rsrq = float(match.group(2))
+                rsrp_mean = rsrp_mean + rsrp
+                rsrq_mean = rsrq_mean + rsrq
+                valid_data = valid_data + 1
+                print("rsrp,rsrq,<rsrp>,<rsrq>",rsrp,rsrq,rsrp_mean/valid_data,rsrq_mean/valid_data)    
+
+                color_strength(rsrp, rsrp_min, rsrp_max, sensitivity)
+                time.sleep(0.5)
+                color_strength(rsrq, rsrq_min, rsrq_max, sensitivity)
+            else:
+                print("no match")
+        else:
+            no_cell = no_cell + 1
+            match = re.search(r'Synchro\sstate\s*:\s*([a-zA-Z0-9_]+[\s]?[a-zA-Z0-9]+)', res) 
+            if match:
+                print(match.group(1))
+            else:
+                print('.',end='')
+
+            if (no_cell > 20): # do something so packets are sent
+                if match.group(1)=="OFF":
+                    sys.exit()
+                #no_cell = 0
+                #res = lte.send_at_cmd('AT+PING="172.31.16.100",1,32')
+        time.sleep(2.0)
 
 def calc_eps():
     """ calculate machine epsilon """
@@ -49,11 +103,11 @@ def sigmoid(x):
     """ sigmoid function, see https://en.wikipedia.org/wiki/Sigmoid_function """
     return 1.0/(1.0 + math.exp(-x))
 
-def color_strength(rsrq, sensitivity=False):
-    """ display signal quality on led blue=bad green=okish red=super """
-    print("rsrq:",rsrq)
+def color_strength(rsrq, minval, maxval, sensitivity=False):
+    """ display signal strength or quality on led blue=bad green=okish red=super """
     # restrict to range -21..-3
-    minval, maxval = -21.0, -3.0 # -12.0 is midpoint
+    # minval, maxval = -21.0, -3.0 # -12.0 is midpoint
+    midpoint = minval + (maxval-minval)/2
     if rsrq < minval:
         rsrq = minval
     if rsrq > maxval:
@@ -62,8 +116,8 @@ def color_strength(rsrq, sensitivity=False):
     colors = [(0, 0, 255), (0, 255, 0), (255, 0, 0)]  # [BLUE, GREEN, RED]
     # calculate color index
     if sensitivity:
-        # apply sensitivity function: shift [-21 , -3] -> [-9 , 9] -> [0 , 1]
-        rsrq_center = rsrq + 12.0
+        # apply sensitivity function: shift [min , max] -> [-val , +val] -> [0 , 1]
+        rsrq_center = rsrq - midpoint
         r, g, b = convert_to_rgb(0.0, 1.0, sigmoid(rsrq_center), colors)
     else:
         r, g, b = convert_to_rgb(minval, maxval, rsrq, colors)
@@ -85,6 +139,6 @@ def convert_to_rgb(minval, maxval, val, colors):
         (r1, g1, b1), (r2, g2, b2) = colors[i], colors[i+1]
         return int(r1 + f*(r2-r1)), int(g1 + f*(g2-g1)), int(b1 + f*(b2-b1))
 
+print("Starting ..")
 time.sleep(2)
-# use sensitivity=True for more sensivity around -12 dB
-signal_quality(show_status=True, sensitivity=False) 
+signal_quality(rsrp_center=-107.8, rsrq_center=-14.7, show_status=True) 
--- readme.md
+++ readme.md
@@ -1,10 +1,58 @@
-# Measure LTE signal strength for FiPy
+# Measure LTE signal strength with FiPy
 
-Will blink the led in colors that indicate LTE signal quality. Green 
-should be just ok (-12 dB), yellow better and red excelent. 
-Blueish green is at the limit, blue is too low for data transfer.
+This script will blink the FiPy led in colors that indicate LTE signal quality. Green
+is the reference value, colors going towards blue represent a weaker signal, yellow
+or red tones represent stronger signals. There are two blinks, the first indicates 
+RSRP and the second RSRQ. For a discussion of these parameters see 
+(https://www.mdpi.com/1424-8220/20/6/1636/htm).
+
+As default values, the follwing windows for the parameters (as defined
+by their center and half width values in function signal_quality()) 
+are used: 
+
+RSRP [ -140 dBm, -90 dBm] center: -110.0 dBm
+RSRQ [ -22.5 dB, -7.5 dB] center:  -15.0 dB
+
+That means that the led is green when the signal is 
+on the center value, blue when the minimal value is 
+obtained and red when the maximal value is returned. 
+
+Values outside of the ranges are treated like the
+max/min values. When the range is smaller, sensitivity around 
+the center is increased.
+
+In order to "calibrate" your ltesignal device, leave it connected
+with the console and run it at a location with "normal" or
+"reference" signal. Leave it running for a while (a couple of 
+minutes) and use the last two values of the last data row (mean 
+values <rsrp> and <rsrq>) 
+
+    rsrp,rsrq,<rsrp>,<rsrq> -107.29 -14.44 -107.7893 -14.724375
+
+as new center values by setting
+
+    signal_quality(rsrp_center=-107.8, rsrq_center=-14.7, show_status=True) 
+
+in the main program. The new reference value will now be shown
+as pure green, a better signal will give more yellowish/redish 
+color tones, a worser signal gives green with blue tones or blue. 
+
+For my antenna (pycom 2016.08.18 rev.02) and location (a small town close 
+to Seville, Spain), values are around -108 dBm and -15 dB at my working place
+which is just on the verge of dropping data.
+
+The modem command does not always return signal strength values,
+so longer pauses between the blinking of the leds are normal.
+It would be easy to add a wifi access point to the code so you can
+see the precise values that are printed to the log
+with a telnet session. You could also easily send them online.
+
+I am just starting to learn LTE so I don't know if this program incurs
+data usage, be careful just in case.
 
 Disable pybytes at startup:
 
-import pycom
-pycom.pybytes_on_boot(False)
+    import pycom
+    pycom.pybytes_on_boot(False)
+
+Have fun!
